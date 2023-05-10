12a4b0a64a8cf4caaf0ba24c7d6ade8cf896436c
Josh <zylo.codes@gmail.com>
2023-05-10 16:32:59 -0500

    response parse

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -2,27 +2,32 @@ defmodule Servy.Handler do
   def handle(request) do
     request
     |> parse
-    |> route
-    |> format_response
+    # |> route
+    # |> format_response
   end
 
   def parse(request) do
-    conv = %{ method: "GET", path: "/wildthings", resp_body: "" }
+    [method, path, _] =
+      request
+      |> String.split("\n")
+      |> List.first
+      |> String.split(" ")
+    %{ method: method, path: path, resp_body: "" }
   end
 
-  def route(conv) do
-    conv = %{ method: "GET", path: "/wildthings", resp_body: "Bears, Lions, Tigers" }
-  end
+  # def route(conv) do
+  #   conv = %{ method: "GET", path: "/wildthings", resp_body: "Bears, Lions, Tigers" }
+  # end
 
-  def format_response(conv) do
-    """
-    HTTP/1.1 200 OK
-    Content-Type: text/html
-    Content-Length: 20
+  # def format_response(conv) do
+  #   """
+  #   HTTP/1.1 200 OK
+  #   Content-Type: text/html
+  #   Content-Length: 20
 
-    Bears, Lions, Tigers
-    """
-  end
+  #   Bears, Lions, Tigers
+  #   """
+  # end
 end
 
 request = """
@@ -34,5 +39,4 @@ Accept: */*
 """
 
 response = Servy.Handler.handle(request)
-
-IO.puts response
+IO.inspect response
